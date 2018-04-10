ce639bc8ca1408f8f7041f55df581910995dc781
Bryan Duerk <bryanduerk@users.noreply.github.com>
2018-04-09 20:33:48 -0400

    commited the wrong changes last time, these are the right ones

--- src/org/usfirst/frc/team467/robot/Logging.java
+++ src/org/usfirst/frc/team467/robot/Logging.java
@@ -20,24 +20,24 @@ public class Logging {
 		setupDefaultLogging();
 
 		// Enable extra logging for classes you want to debug
-//		Logger.getLogger(Action.class).setLevel(Level.INFO);
+		//		Logger.getLogger(Action.class).setLevel(Level.INFO);
 		Logger.getLogger(Drive.class).setLevel(Level.DEBUG);
-        Logger.getLogger(ActionGroup.class).setLevel(Level.INFO);
-        Logger.getLogger("telemetry").setLevel(Level.INFO);
-//		Logger.getLogger(DriveSimulator.class).setLevel(Level.WARN);
-//		Logger.getLogger(Elevator.class).setLevel(Level.INFO);
-//		Logger.getLogger(Grabber.class).setLevel(Level.INFO);
-//		Logger.getLogger(MatchConfiguration.class).setLevel(Level.INFO);
-//		Logger.getLogger(OpticalSensor.class).setLevel(Level.WARN);
-//		Logger.getLogger(Ramp.class).setLevel(Level.INFO);
-//		Logger.getLogger(Ramps.class).setLevel(Level.INFO);
-//		Logger.getLogger(Robot.class).setLevel(Level.INFO);
-//		Logger.getLogger(RobotShape.class).setLevel(Level.WARN);
-//		Logger.getLogger(Rumbler.class).setLevel(Level.WARN);
-//		Logger.getLogger(TalonSpeedControllerGroup.class).setLevel(Level.INFO);
-//		Logger.getLogger(VisionIntegration.class).setLevel(Level.WARN);
-//		Logger.getLogger(XBoxJoystick467.class).setLevel(Level.WARN);
-//		Logger.getLogger(TiltMonitor.class).setLevel(Level.INFO);
+		Logger.getLogger(ActionGroup.class).setLevel(Level.INFO);
+		Logger.getLogger("telemetry").setLevel(Level.INFO);
+		//		Logger.getLogger(DriveSimulator.class).setLevel(Level.WARN);
+		//		Logger.getLogger(Elevator.class).setLevel(Level.INFO);
+		//		Logger.getLogger(Grabber.class).setLevel(Level.INFO);
+		//		Logger.getLogger(MatchConfiguration.class).setLevel(Level.INFO);
+		//		Logger.getLogger(OpticalSensor.class).setLevel(Level.WARN);
+		//		Logger.getLogger(Ramp.class).setLevel(Level.INFO);
+		//		Logger.getLogger(Ramps.class).setLevel(Level.INFO);
+		//		Logger.getLogger(Robot.class).setLevel(Level.INFO);
+		//		Logger.getLogger(RobotShape.class).setLevel(Level.WARN);
+		//		Logger.getLogger(Rumbler.class).setLevel(Level.WARN);
+		//		Logger.getLogger(TalonSpeedControllerGroup.class).setLevel(Level.INFO);
+		//		Logger.getLogger(VisionIntegration.class).setLevel(Level.WARN);
+		//		Logger.getLogger(XBoxJoystick467.class).setLevel(Level.WARN);
+		//		Logger.getLogger(TiltMonitor.class).setLevel(Level.INFO);
 	}
 
 	private static void setupDefaultLogging() {
@@ -49,30 +49,35 @@ public class Logging {
 		Logger.getRootLogger().addAppender(new ConsoleAppender(layout));
 		String logDirectory = "/home/admin/log/";
 		String logFileName = "Robot467.log";
+		boolean useUsb = false;
 		// Set default string for layout location
 		// Try to open and read a file on the USB.
 		// IF no file/USB, then log in catch
 		// if File exists, change layout location to USB
-		
+
 		String usbDirectory = "/media/sda1/";
 		File file = new File(usbDirectory +"RobotDummyFile.txt");
 		if (file.exists()) {
 			logDirectory = usbDirectory;
+			useUsb = true;
 		}
-
 		
 		try {
 			System.out.println("printing file at: " + logDirectory + logFileName); 
 			RollingFileAppender rollingFileAppender = new RollingFileAppender(layout, logDirectory + logFileName);
-			rollingFileAppender.setMaxBackupIndex(20);
-			rollingFileAppender.setMaximumFileSize(1_000_000);
+			if(useUsb) {
+				rollingFileAppender.setMaxBackupIndex(100);
+				rollingFileAppender.setMaximumFileSize(10_000_000);
+			} else {		
+				rollingFileAppender.setMaxBackupIndex(20);
+				rollingFileAppender.setMaximumFileSize(1_000_000);
+			}
 			rollingFileAppender.rollOver();
 			Logger.getRootLogger().addAppender(rollingFileAppender);
 		} catch (IOException e) {
 			System.out.println("Failed to create log file appender: " + e.getMessage());
 		}
-
-
+		
 		// Set the default log level to INFO.
 		Logger.getRootLogger().setLevel(Level.ERROR); // changing log level
 	}
