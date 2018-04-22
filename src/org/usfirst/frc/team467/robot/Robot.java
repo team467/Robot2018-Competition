15003a504ff5548f50e8820f65755e09fa5dbd2a
mahirsowad3 <32722691+mahirsowad3@users.noreply.github.com>
2018-04-22 13:35:37 -0400

    Ready for testing with grabber and cube

--- src/org/usfirst/frc/team467/robot/Robot.java
+++ src/org/usfirst/frc/team467/robot/Robot.java
@@ -156,7 +156,7 @@ public class Robot extends TimedRobot {
 		elevator.move(driverstation.getElevatorSpeed());
 		drive.setRamp(elevator.getHeight());
 		
-		leds.blink();
+		leds.act();
 		
 		if (driverstation.getFloorHeightButtonPressed()) {
 			LOGGER.info("Dropping to bottom height");
