2fffad41063e3c1e97eb6e32c5fe35ae1478344e
HexCodeZero <32725385+HexCodeZero@users.noreply.github.com>
2018-04-27 07:52:48 -0400

    Decreased climb speed

--- src/org/usfirst/frc/team467/robot/Climber.java
+++ src/org/usfirst/frc/team467/robot/Climber.java
@@ -7,7 +7,6 @@ import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.MotorSafetyHelper;
 
 
 public class Climber {
