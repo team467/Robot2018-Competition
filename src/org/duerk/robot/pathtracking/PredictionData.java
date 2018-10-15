24cd347987f388cbc39df2f0fa1f1a574e6ef29e
Bryan Duerk <bryanduerk@users.noreply.github.com>
2018-10-15 18:49:32 -0400

    Added some path tracking files. LQR isn't finished. The others work by themselves, but need to be modified to work with the robot.

---
+++ src/org/duerk/robot/pathtracking/PredictionData.java
@@ -0,0 +1,9 @@
+package org.duerk.robot.pathtracking;
+
+public class PredictionData {
+    public double[] time;
+    public double[] x;
+    public double[] y;
+    public double[] yaw;
+    public double[] velocity;
+}
